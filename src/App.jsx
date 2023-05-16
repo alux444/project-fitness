8ba344dc32ba4c82a9628384c14d20f648d31184
Alex Liang <alex@Alexs-MacBook-Air.local>
2023-05-16 21:42:59 +1200

    github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import ExercisePage from "./components/ExercisePage";
 import SelectExercises from "./components/SelectExercises";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SelectExercises />} />
-          <Route path="/exercise/:id" element={<ExercisePage />} />
-        </Routes>
-      </Router>
+      <SelectExercises />
     </>
   );
 }
