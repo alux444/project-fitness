95fc2f0b65245a53f97e82a64c2fb1d25f132d64
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-05-18 19:09:23 +1200

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="footer">
       <hr></hr>
       <p>Find us here</p>
-      <p><a href="https://github.com/alux444">alux444</a> and <a href="https://github.com/ckim349">ckim349</a></p>
+      <p><a href="https://github.com/alux444" target="_blank">alux444</a> and <a href="https://github.com/ckim349" target="_blank">ckim349</a></p>
       <small>© 2023 Alex and Chulshin</small>
     </div>
   );
